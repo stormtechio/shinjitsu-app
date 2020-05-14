e563e543156723b41eca94bd8c24fbb260df633d
Ayrton Sousa <ayrton@samex.online>
2020-05-14 12:02:51 -0300

    Atualizacao
    Foi efetuado uma troca de imports  no xml e no .java, android.support.v7.widget.Toolbar; foi substituido por androidx.appcompat.widget.Toolbar  que corresponde à herança feita na InitialActivity

--- Shinjitsu/app/src/main/java/com/example/shinjitsu/InitialActivity.java
+++ Shinjitsu/app/src/main/java/com/example/shinjitsu/InitialActivity.java
@@ -1,22 +1,23 @@
 package com.example.shinjitsu;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.widget.Toolbar;
+
 
 public class InitialActivity extends AppCompatActivity {
 
    // Toolbar toolbar;
-    android.support.v7.widget.Toolbar toolbar;
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_initial);
 
-        toolbar = (android.support.v7.widget.Toolbar) findViewById(R.id.my_toolbar);
-       // setSupportActionBar(toolbar);
+        toolbar = findViewById(R.id.my_toolbar);
+        setSupportActionBar(toolbar);
     }
 }
