36a0b4e5c9ca34d40c8cd2729f09c5d20e9e188d
Saori Costa <saoricosta@gmail.com>
2020-05-14 17:45:06 -0300

    Tela de toolbar

--- Shinjitsu/app/src/main/java/com/example/shinjitsu/InitialActivity.java
+++ Shinjitsu/app/src/main/java/com/example/shinjitsu/InitialActivity.java
@@ -1,22 +1,24 @@
 package com.example.shinjitsu;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.widget.Toolbar;
+//import android.support.v7.widget.Toolbar;
 
 public class InitialActivity extends AppCompatActivity {
 
+    Toolbar toolbar;
    // Toolbar toolbar;
-    android.support.v7.widget.Toolbar toolbar;
+   // android.support.v7.widget.Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_initial);
 
-        toolbar = (android.support.v7.widget.Toolbar) findViewById(R.id.my_toolbar);
-       // setSupportActionBar(toolbar);
+        toolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(toolbar);
     }
 }
