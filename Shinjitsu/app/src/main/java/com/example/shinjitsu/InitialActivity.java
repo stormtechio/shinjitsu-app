038f9e452dbae66fccb46d71238e01a9dde6156a
Saori Costa <saoricosta@gmail.com>
2020-05-21 11:40:01 -0300

    Criacao de menu

--- Shinjitsu/app/src/main/java/com/example/shinjitsu/InitialActivity.java
+++ Shinjitsu/app/src/main/java/com/example/shinjitsu/InitialActivity.java
@@ -1,17 +1,14 @@
 package com.example.shinjitsu;
 
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.drawerlayout.widget.DrawerLayout;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-
-import android.content.Intent;
-import android.os.Bundle;
-import android.view.MenuItem;
-import android.widget.Toast;
 
 import com.google.android.material.navigation.NavigationView;
 
@@ -32,7 +29,7 @@ public class InitialActivity extends AppCompatActivity {
 //        setSupportActionBar(toolbar);
 
 
-        dl = (DrawerLayout)findViewById(R.id.activity_main);
+        dl = (DrawerLayout)findViewById(R.id.activity_initial);
         t = new ActionBarDrawerToggle(this, dl,R.string.Open, R.string.Close);
 
         dl.addDrawerListener(t);
