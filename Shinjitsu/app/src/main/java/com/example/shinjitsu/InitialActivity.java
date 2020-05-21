a603cefb6eb42ba82c182124509b3ac1a605f4a9
Saori Costa <saoricosta@gmail.com>
2020-05-21 14:09:13 -0300

    Criacao de menu

--- Shinjitsu/app/src/main/java/com/example/shinjitsu/InitialActivity.java
+++ Shinjitsu/app/src/main/java/com/example/shinjitsu/InitialActivity.java
@@ -7,72 +7,60 @@ import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
 import androidx.drawerlayout.widget.DrawerLayout;
 
 import com.google.android.material.navigation.NavigationView;
 
 public class InitialActivity extends AppCompatActivity {
 
-    Toolbar toolbar;
-    private DrawerLayout dl;
-    private ActionBarDrawerToggle t;
-    private NavigationView nv;
+    private DrawerLayout drawerLayout;
+    private ActionBarDrawerToggle actionBarDrawerToggle;
+    private NavigationView navigationView;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_initial);
-//
-//        toolbar = findViewById(R.id.my_toolbar);
-//        setSupportActionBar(toolbar);
 
+        drawerLayout = (DrawerLayout)findViewById(R.id.activity_initial);
+        actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout,R.string.Open, R.string.Close);
 
-        dl = (DrawerLayout)findViewById(R.id.activity_initial);
-        t = new ActionBarDrawerToggle(this, dl,R.string.Open, R.string.Close);
-
-        dl.addDrawerListener(t);
-        t.syncState();
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        nv = (NavigationView)findViewById(R.id.nv);
-        nv.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+        navigationView = (NavigationView)findViewById(R.id.navigation);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                 int id = item.getItemId();
                 switch(id)
                 {
-                    case R.id.account:
-                        Toast.makeText(getApplicationContext(), "My Account",Toast.LENGTH_SHORT).show();break;
-                    case R.id.settings:
-                        Toast.makeText(getApplicationContext(), "Settings",Toast.LENGTH_SHORT).show();break;
-                    case R.id.mycart:
-                        Toast.makeText(getApplicationContext(), "My Cart",Toast.LENGTH_SHORT).show();break;
+                    case R.id.student:
+                        Toast.makeText(getApplicationContext(), "Alunos",Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.payment:
+                        Toast.makeText(getApplicationContext(), "Pagamentos",Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.option:
+                        Toast.makeText(getApplicationContext(), "Opções",Toast.LENGTH_SHORT).show();
+                        break;
                     default:
                         return true;
                 }
-
-
                 return true;
-
             }
         });
-
-
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
 
-        if(t.onOptionsItemSelected(item))
+        if(actionBarDrawerToggle.onOptionsItemSelected(item))
             return true;
 
         return super.onOptionsItemSelected(item);
     }
-
-
-
-    //}
 }
