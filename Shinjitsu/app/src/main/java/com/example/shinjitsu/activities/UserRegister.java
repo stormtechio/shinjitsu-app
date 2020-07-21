afae5bedfcae06dc876aad1d0044efd1a85885fd
Saori Costa <saoricosta@gmail.com>
2020-07-21 10:27:31 -0300

    Formulario de usuario

---
+++ Shinjitsu/app/src/main/java/com/example/shinjitsu/activities/UserRegister.java
@@ -0,0 +1,21 @@
+package com.example.shinjitsu.activities;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.shinjitsu.R;
+
+public class UserRegister extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_register);
+
+
+
+    }
+}
