8443e45e7ac8e320304dfeefc26936f6c51e030f
Saori Costa <saoricosta@gmail.com>
2020-08-05 14:53:44 -0300

    Retrofit

--- Shinjitsu/app/src/main/java/com/example/shinjitsu/interfaces/WebService.java
+++ Shinjitsu/app/src/main/java/com/example/shinjitsu/interfaces/WebService.java
@@ -8,7 +8,7 @@ import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.Path;
 
-public interface GitHubService {
+public interface WebService {
 
     @GET("students/{student}/repos")
     Call<List<StudentRepository>> listRepos(@Path("student") String student);
